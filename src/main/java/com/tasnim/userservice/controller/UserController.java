9bfcfd68a22872337a2b8bee54e5aaada96ca3fc
tasnimferdous <tasnimferdous216@gmail.com>
2026-05-05 23:58:57 +0600

    exposing user info

--- src/main/java/com/tasnim/userservice/controller/UserController.java
+++ src/main/java/com/tasnim/userservice/controller/UserController.java
@@ -3,19 +3,20 @@ package com.tasnim.userservice.controller;
 
 import com.tasnim.userservice.dto.UserRequestDto;
 import com.tasnim.userservice.enums.Role;
+import com.tasnim.userservice.service.CustomUserDetailsService;
 import com.tasnim.userservice.service.UserService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class UserController {
     private final UserService userService;
+    private final CustomUserDetailsService customUserDetailsService;
 
-    public UserController(UserService userService) {
+    public UserController(UserService userService, CustomUserDetailsService customUserDetailsService) {
         this.userService = userService;
+        this.customUserDetailsService = customUserDetailsService;
     }
 
     @PostMapping("/user/register")
@@ -28,5 +29,10 @@ public class UserController {
     @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<?> registerAdmin(@RequestBody UserRequestDto userRequestDto){
         return ResponseEntity.ok(userService.registerUser(userRequestDto));
+    }
+
+    @GetMapping("/user/info/{username}")
+    public ResponseEntity<?> getUserInfo(@PathVariable String username){
+        return ResponseEntity.ok(customUserDetailsService.loadUserByUsername(username));
     }
 }
